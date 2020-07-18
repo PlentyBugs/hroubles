495ba7a52d109149d4290f8e6ce88e078fc6f257
PlentyBugs <43182741+PlentyBugs@users.noreply.github.com>
2020-07-18 17:41:43 +0300

    add getRequest for product

--- src/main/java/org/hroubles/mir/controller/ProductController.java
+++ src/main/java/org/hroubles/mir/controller/ProductController.java
@@ -1,5 +1,10 @@
 package org.hroubles.mir.controller;
 
+import org.hroubles.mir.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,8 +14,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/product")
 public class ProductController {
 
+    @Autowired
+    private ProductRepository productRepository;
+
     @GetMapping
-    public String productList(Model model) {
+    public String productList(
+            Model model,
+            @PageableDefault(sort = {"id"}, direction = Sort.Direction.DESC) Pageable pageable
+    ) {
+        model.addAttribute("page", productRepository.findAll(pageable));
         return "productList";
     }
 }
