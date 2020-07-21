f3e82600021d74bf21beb99e1ef20508c324c134
PlentyBugs <43182741+PlentyBugs@users.noreply.github.com>
2020-07-21 15:16:13 +0300

    move password encoder

--- src/main/java/org/hroubles/mir/config/WebSecurityConfig.java
+++ src/main/java/org/hroubles/mir/config/WebSecurityConfig.java
@@ -1,14 +1,12 @@
 package org.hroubles.mir.config;
 
 import org.hroubles.mir.service.UserService;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
@@ -39,11 +37,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                     .logout()
                     .permitAll();
-    }
-
-    @Bean
-    public PasswordEncoder getPasswordEncoder() {
-        return new BCryptPasswordEncoder(8);
     }
 
     @Override
