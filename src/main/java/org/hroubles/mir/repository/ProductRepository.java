10342d9181b3ed49e4949ca2bdddbf118bc9eef7
PlentyBugs <43182741+PlentyBugs@users.noreply.github.com>
2020-07-22 20:38:25 +0300

    fix bug with search and pages

--- src/main/java/org/hroubles/mir/repository/ProductRepository.java
+++ src/main/java/org/hroubles/mir/repository/ProductRepository.java
@@ -9,14 +9,19 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Set;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Page<Product> findAll(Pageable pageable);
 
     @Query("from Product p where :tag member of p.tags")
     Page<Product> findByTag(@Param("tag") Tag tag, Pageable pageable);
 
+    @Query("from Product p where :tags member of p.tags")
+    Page<Product> findByTags(@Param("tags") Set<Tag> tags, Pageable pageable);
+
     @Query("from Product p where lower(p.description) like lower(:str) or lower(p.name) like lower(:str) or lower(p.seller.username) like lower(:str)")
-    Page<Product> findByDescriptionOrNameOrSellerUsernameContains(String str, Pageable pageable);
+    Page<Product> findByDescriptionOrNameOrSellerUsernameOrTagsContains(String str, Pageable pageable);
 
     Page<Product> findBySeller(User seller, Pageable pageable);
 }
--- src/main/java/org/hroubles/mir/service/ProductService.java
+++ src/main/java/org/hroubles/mir/service/ProductService.java
@@ -4,12 +4,11 @@ import org.hroubles.mir.domain.Product;
 import org.hroubles.mir.domain.enums.Tag;
 import org.hroubles.mir.repository.ProductRepository;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.Optional;
+import java.util.Set;
 
 @Service
 public class ProductService {
@@ -25,37 +24,11 @@ public class ProductService {
     }
 
     public Page<Product> searchByTags(Set<Tag> tags, Pageable pageable) {
-        List<Page<Product>> pages = new ArrayList<>();
-        for (Tag tag : tags) pages.add(searchByTag(tag, pageable));
-        return merge(pages, pageable);
+        return productRepository.findByTags(tags, pageable);
     }
 
     public Page<Product> search(String str, Pageable pageable) {
-        List<Page<Product>> pages = new ArrayList<>();
-        Set<String> allTags = Arrays.stream(Tag.values()).map(Tag::name).collect(Collectors.toSet());
-        Set<Tag> tags = new HashSet<>();
-        String[] words = str.split("\\s+");
-        for (String word : words) {
-            String upperWord = word.toUpperCase();
-            if (allTags.contains(upperWord)) {
-                tags.add(Tag.valueOf(upperWord));
-            }
-            pages.add(productRepository.findByDescriptionOrNameOrSellerUsernameContains("%" + word + "%", pageable));
-        }
-        pages.add(searchByTags(tags, pageable));
-        return merge(pages, pageable);
-    }
-
-    private Page<Product> merge(List<Page<Product>> pages, Pageable pageable) {
-
-        Set<Product> products = new HashSet<>();
-        for (Page<Product> page : pages) {
-            products.addAll(page.toSet());
-        }
-
-        List<Product> resProducts = new ArrayList<>(products);
-
-        return new PageImpl<>(resProducts, pageable, resProducts.size());
+        return productRepository.findByDescriptionOrNameOrSellerUsernameOrTagsContains("%" + str + "%", pageable);
     }
 
     public Page<Product> findAll(Pageable pageable) {
