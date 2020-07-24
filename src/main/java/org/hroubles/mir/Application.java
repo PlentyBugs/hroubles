2df15fc3d891191c240e2eea9c88b19fbad4a269
PlentyBugs <43182741+PlentyBugs@users.noreply.github.com>
2020-07-24 16:30:24 +0300

    added some annotations and made mail sending async

--- src/main/java/org/hroubles/mir/Application.java
+++ src/main/java/org/hroubles/mir/Application.java
@@ -3,9 +3,11 @@ package org.hroubles.mir;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableJpaRepositories("org.hroubles.mir.repository")
+@EnableAsync
 public class Application {
 
     public static void main(String[] args) {
